390f74ced0576840f310c0034e899454167744bb
Adrian Anslow <adriananslow@hotmail.com>
2022-01-26 11:52:05 +0000

    amend deck class shuffle method to use built in method from random module

--- deck.py
+++ deck.py
@@ -10,10 +10,7 @@ class Deck:
         return [Card(suit, value) for suit in Card.SUIT_SYMBOLS for value in Card.VALUE_NAMES]
 
     def shuffle(self):
-        rand_values = [random.random() for _ in range(52)]
-        rand_deck = zip(self.cards, rand_values)
-        shuffled_rand_deck = sorted(rand_deck, key = lambda x: x[1])
-        self.cards = [item[0] for item in shuffled_rand_deck]
+        random.shuffle(self.cards)
 
     def deal(self, num_cards):
         dealt_cards = self.cards[:num_cards]
